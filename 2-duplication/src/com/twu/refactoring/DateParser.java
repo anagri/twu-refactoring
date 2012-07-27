0b8b236df8e9cd867be7a0b5e6cdb0598ae3de89
Amiruddin Nagri <amir.nagri+git@gmail.com>
2012-07-27 14:09:40 +0530

    amir - removing duplicated method and reusing parameterized methods

--- 2-duplication/src/com/twu/refactoring/DateParser.java
+++ 2-duplication/src/com/twu/refactoring/DateParser.java
@@ -2,7 +2,6 @@ package com.twu.refactoring;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.TimeZone;
 
 public class DateParser {
@@ -24,65 +23,24 @@ public class DateParser {
     public Date parse() {
         int year, month, date, hour, minute;
 
-        try {
-            String yearString = dateAndTimeString.substring(0, 4);
-            year = Integer.parseInt(yearString);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Year string is less than 4 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Year is not an integer");
-        }
-        if (year < 2000 || year > 2012)
-            throw new IllegalArgumentException("Year cannot be less than 2000 or more than 2012");
+        year = parse(0, 4, "Year");
+        checkRange(year, 2000, 2012, "Year");
 
-        try {
-            String monthString = dateAndTimeString.substring(5, 7);
-            month = Integer.parseInt(monthString);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Month string is less than 2 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Month is not an integer");
-        }
-        if (month < 1 || month > 12)
-            throw new IllegalArgumentException("Month cannot be less than 1 or more than 12");
+        month = parse(5, 7, "Month");
+        checkRange(month, 1, 12, "Month");
 
-        try {
-            String dateString = dateAndTimeString.substring(8, 10);
-            date = Integer.parseInt(dateString);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Date string is less than 2 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Date is not an integer");
-        }
-        if (date < 1 || date > 31)
-            throw new IllegalArgumentException("Date cannot be less than 1 or more than 31");
+        date = parse(8, 10, "Date");
+        checkRange(date, 1, 31, "Date");
 
         if (dateAndTimeString.substring(11, 12).equals("Z")) {
             hour = 0;
             minute = 0;
         } else {
-            try {
-                String hourString = dateAndTimeString.substring(11, 13);
-                hour = Integer.parseInt(hourString);
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new IllegalArgumentException("Hour string is less than 2 characters");
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Hour is not an integer");
-            }
-            if (hour < 0 || hour > 23)
-                throw new IllegalArgumentException("Hour cannot be less than 0 or more than 23");
+            hour = parse(11, 13, "Hour");
+            checkRange(hour, 0, 23, "Hour");
 
-            try {
-                String minuteString = dateAndTimeString.substring(14, 16);
-                minute = Integer.parseInt(minuteString);
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new IllegalArgumentException("Minute string is less than 2 characters");
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Minute is not an integer");
-            }
-            if (minute < 0 || minute > 59)
-                throw new IllegalArgumentException("Minute cannot be less than 0 or more than 59");
-
+            minute = parse(14, 16, "Minute");
+            checkRange(minute, 0, 59, "Minute");
         }
 
         Calendar calendar = Calendar.getInstance();
@@ -90,5 +48,21 @@ public class DateParser {
         calendar.set(year, month - 1, date, hour, minute, 0);
         calendar.set(Calendar.MILLISECOND, 0);
         return calendar.getTime();
+    }
+
+    private void checkRange(int value, int minValue, int maxValue, String fieldName) {
+        if (value < minValue || value > maxValue)
+            throw new IllegalArgumentException(fieldName + " cannot be less than " + minValue + " or more than " + maxValue);
+    }
+
+    private int parse(int fromIndex, int toIndex, String fieldName) {
+        try {
+            String yearString = dateAndTimeString.substring(fromIndex, toIndex);
+            return Integer.parseInt(yearString);
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(fieldName + " string is less than " + (toIndex - fromIndex) + " characters");
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(fieldName + " is not an integer");
+        }
     }
 }
